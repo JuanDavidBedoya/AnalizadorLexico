cb41c17b5bd38726bfd55865b8d47fd85762ecaf
JuanMartinezB <juans.martinezb@uqvirtual.edu.co>
2025-10-28 13:31:03 -0500

    Corrección du un método que estaba incorrecto en extraerOperadorAritmetico

--- js/modelo/AnalizadorLexico.js
+++ js/modelo/AnalizadorLexico.js
@@ -195,7 +195,7 @@ export class AnalizadorLexico {
         const operadoresSimples = ['+', '-', '*', '/', '%'];
 
         const siguiente = this.caracterSiguiente();
-        if ((char === '+' && siguiente === '+') || (char === '-' && siguiente === '-')) {
+        if ((char === '~' && siguiente === '/')) {
             const lexema = char + siguiente;
             this.avanzar(); 
             this.avanzar();
